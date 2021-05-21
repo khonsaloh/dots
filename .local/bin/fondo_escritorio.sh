e2db3adacb021ffb7df6cf885471e3b855c4030b
gon <54636165+khonsaloh@users.noreply.github.com>
2021-05-21 08:00:35 +0200

    20210521080035

--- .local/bin/fondo_escritorio.sh
+++ .local/bin/fondo_escritorio.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 waldir=/usr/share/wallpaper/
-case $1 in
+[ -n "$1" ] && case $1 in
 	"aleatorio") xwallpaper --zoom "$(find $waldir \
 		-maxdepth 1 -type f | shuf -n 1)" ;;
 	"fijo") xwallpaper --zoom "$(cat "$HOME"/.local/share/fondo.txt)" ;;
+	*) xwallpaper --zoom "$(/usr/bin/find /usr/share/wallpaper/ -maxdepth 1 -type f |
+		sxiv -tio 2>/dev/null)" 2>/dev/null & ;;
 esac
 
 [ -n "$1" ] && exit
@@ -18,7 +20,7 @@ EOF
 printf "opcion: " && read -r fondo
 
 case $fondo in
-	"1") xwallpaper --zoom "$(find $waldir -maxdepth 1 -type f | fzf --cycle --reverse)" 2>/dev/null ;;
+	"1") xwallpaper --zoom "$(sxiv $waldir -to)" 2>/dev/null ;;
 	"2")
 	while true; do
 		xwallpaper --zoom "$(find $waldir -maxdepth 1 -type f | shuf -n 1)"
